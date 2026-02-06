55befc1835e9ba966e53ca4c970402b85a5374e0
kenjic95 <kenjinescorpuz@gmail.com>
2026-02-06 22:50:19 +1100

    9.2.1

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {
-  createStackNavigator,
   TransitionPresets,
+  createStackNavigator,
 } from "@react-navigation/stack";
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
 import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
