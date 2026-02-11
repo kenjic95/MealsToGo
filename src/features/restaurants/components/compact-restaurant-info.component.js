c38b08d85c673a215656a315785a90a426e5a01a
kenjic95 <kenjinescorpuz@gmail.com>
2026-02-11 13:21:11 +1100

    10.5 Custom Map Callout Function

---
+++ src/features/restaurants/components/compact-restaurant-info.component.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components/native";
+import WebView from "react-native-webview";
+import { Platform } from "react-native";
+
+import { Text } from "../typography/text.component";
+
+const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebview = styled(WebView)`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
